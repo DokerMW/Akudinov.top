ad39ed749bca849e37459c957fba2352fdbea12f
DokerMW <abddoker@gmail.com>
2024-11-09 01:14:26 +0500

    add: complete client html and scss. + PixelPerfect + Slider

--- _src/js/_sliders.js
+++ _src/js/_sliders.js
@@ -1,12 +1,26 @@
 const swiper = new Swiper('.examples__swiper', {
-	// Optional parameters
 	direction: 'horizontal',
 	loop: false,
 	slidesPerView: 3,
 	spaceBetween: 20,
-	// Navigation arrows
 	navigation: {
 		nextEl: '.examples__next-btn',
 		prevEl: '.examples__prev-btn',
+	},
+});
+
+
+const swiperTwo = new Swiper('.clients__swiper', {
+	direction: 'horizontal',
+	loop: false,
+	slidesPerView: 6,
+	spaceBetween: 55,
+	autoHeight: false,
+	pagination: {
+		el: '.swiper-pagination',
+	},
+	navigation: {
+		nextEl: '.clients__next-btn',
+		prevEl: '.clients__prev-btn',
 	},
 });
--- js/script.js
+++ js/script.js
@@ -1,16 +1,31 @@
 
 const swiper = new Swiper('.examples__swiper', {
-	// Optional parameters
 	direction: 'horizontal',
 	loop: false,
 	slidesPerView: 3,
 	spaceBetween: 20,
-	// Navigation arrows
 	navigation: {
 		nextEl: '.examples__next-btn',
 		prevEl: '.examples__prev-btn',
 	},
 });
+
+
+const swiperTwo = new Swiper('.clients__swiper', {
+	direction: 'horizontal',
+	loop: false,
+	slidesPerView: 6,
+	spaceBetween: 55,
+	autoHeight: false,
+	pagination: {
+		el: '.swiper-pagination',
+	},
+	navigation: {
+		nextEl: '.clients__next-btn',
+		prevEl: '.clients__prev-btn',
+	},
+});
+
 new Accordion('.accordion-container', {
 	duration: 400,
 	showMultiple: true,
