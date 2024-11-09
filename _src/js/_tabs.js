9e60b87a856f623e6bf57da663cbd952d420c3ee
DokerMW <abddoker@gmail.com>
2024-11-09 12:03:51 +0500

    add: tabs script

--- _src/js/_tabs.js
+++ _src/js/_tabs.js
@@ -1,35 +1,3 @@
-
-const swiper = new Swiper('.examples__swiper', {
-	direction: 'horizontal',
-	loop: false,
-	slidesPerView: 3,
-	spaceBetween: 20,
-	navigation: {
-		nextEl: '.examples__next-btn',
-		prevEl: '.examples__prev-btn',
-	},
-});
-
-
-const swiperTwo = new Swiper('.clients__swiper', {
-	direction: 'horizontal',
-	loop: false,
-	slidesPerView: 6,
-	spaceBetween: 55,
-	autoHeight: false,
-	pagination: {
-		el: '.swiper-pagination',
-	},
-	navigation: {
-		nextEl: '.clients__next-btn',
-		prevEl: '.clients__prev-btn',
-	},
-});
-
-new Accordion('.accordion-container', {
-	duration: 400,
-	showMultiple: true,
-});
 const tabBtns = document.querySelectorAll('.tabs__btn');
 const videoTab = document.getElementById('video')
 const textTab = document.getElementById('text')
--- _src/js/script.js
+++ _src/js/script.js
@@ -1,2 +1,3 @@
 //@prepros-append _sliders.js
 //@prepros-append _accordion.js
+//@prepros-append _tabs.js
