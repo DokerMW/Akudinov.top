84fc48dcfbd1ee8eda8f619904a7173379a51633
DokerMW <abddoker@gmail.com>
2024-11-10 13:47:53 +0500

    add: error label on form

--- js/script.js
+++ js/script.js
@@ -246,45 +246,73 @@ forms.forEach(e => {
 		let inputName = event.target.querySelector('input[name="name"]');
 		let inputEmail = event.target.querySelector('input[name="email"]');
 		let inputDomen = event.target.querySelector('input[name="domen"]');
-		if (inputEmail) {
-			if (testEmail.test(inputEmail.value)) {
-				inputEmail.classList.add('success')
-			} else {
-				event.preventDefault();
-				inputEmail.classList.add('error')
+
+		const validate = (inputName, test) => {
+			if (inputName) {
+				if (test.test(inputName.value)) {
+					inputName.classList.add('success')
+				} else {
+					let parent = inputName.closest('.input');
+					event.preventDefault();
+					inputName.classList.add('error')
+					parent.classList.add('error')
+				}
 			}
 		}
-		if (inputTel) {
-			if (testPhone.test(inputTel.value)) {
-				inputTel.classList.add('success')
-			} else {
-				event.preventDefault();
-				inputTel.classList.add('error')
-			}
-		}
-		if (inputName) {
-			if (testName.test(inputName.value)) {
-				inputName.classList.add('success')
-			} else {
-				event.preventDefault();
-				inputName.classList.add('error')
-			}
-		}
-		if (inputDomen) {
-			if (testDomen.test(inputDomen.value)) {
-				inputDomen.classList.add('success')
-			} else {
-				event.preventDefault();
-				inputDomen.classList.add('error')
-			}
-		}
+
+		validate(inputEmail, testEmail)
+		validate(inputTel, testPhone)
+		validate(inputName, testName)
+		validate(inputDomen, testDomen)
+		// if (inputEmail) {
+		// 	if (testEmail.test(inputEmail.value)) {
+		// 		inputEmail.classList.add('success')
+		// 	} else {
+		// 		let parent = inputEmail.closest('.input');
+		// 		event.preventDefault();
+		// 		inputEmail.classList.add('error')
+		// 		parent.classList.add('error')
+		// 	}
+		// }
+		// if (inputTel) {
+		// 	if (testPhone.test(inputTel.value)) {
+		// 		inputTel.classList.add('success')
+		// 	} else {
+		// 		let parent = inputTel.closest('.input');
+		// 		event.preventDefault();
+		// 		inputTel.classList.add('error')
+		// 		parent.classList.add('error')
+		// 	}
+		// }
+		// if (inputName) {
+		// 	if (testName.test(inputName.value)) {
+		// 		inputName.classList.add('success')
+		// 	} else {
+		// 		let parent = inputName.closest('.input');
+		// 		event.preventDefault();
+		// 		inputName.classList.add('error')
+		// 		parent.classList.add('error')
+		// 	}
+		// }
+		// if (inputDomen) {
+		// 	if (testDomen.test(inputDomen.value)) {
+		// 		inputDomen.classList.add('success')
+		// 	} else {
+		// 		let parent = inputDomen.closest('.input');
+		// 		event.preventDefault();
+		// 		inputDomen.classList.add('error')
+		// 		parent.classList.add('error')
+		// 	}
+		// }
 	});
 })
 
 allInputs.forEach(e => {
 	e.addEventListener('focus', event => {
 		if (event.target.classList.contains('error')) {
+			let parent = event.target.closest('.input');
 			event.target.classList.remove('error')
+			parent.classList.remove('error')
 		}
 	});
 })
